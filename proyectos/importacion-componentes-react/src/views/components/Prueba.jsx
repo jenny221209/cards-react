b8eda6c1230f39d8103f7b65153f6b7609c827b4
jenny alexandra gomez parra <jenny16parr@gmail.com>
2020-06-17 11:57:51 -0500

    New: proyecto - prueba

---
+++ proyectos/importacion-componentes-react/src/views/components/Prueba.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Componentes Material UI
+import Button from '@material-ui/core/Button';
+
+// Componentes Material Kit
+//import ButtonMT from '../../components/CustomButtons/Button';
+
+function Prueba () {
+    return (
+        <div>
+        <Button variant="contained" color="secondary">
+        Secondary
+        </Button>
+
+        {/*<ButtonMT type="button" color="warning">Warning</ButtonMT>*/}
+        </div>
+    );
+}
+ export default Prueba;
